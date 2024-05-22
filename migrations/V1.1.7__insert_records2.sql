cc557a31134a4c7faef1bbfdab8cb9bdfac7afc9
Jasraj Gill <jasrajgill2752@gmail.com>
2024-05-22 15:38:20 +0930

    two adds

---
+++ migrations/V1.1.7__insert_records2.sql
@@ -0,0 +1,6 @@
+USE SCHEMA DEMO;
+INSERT INTO HELLO_WORLD 
+VALUES ('Bob', 'Williams', 35, 'M', 'bob.williams@@@XYZ.com');
+
+INSERT INTO HELLO_WORLD 
+VALUES ('Charlie', 'Brown', 37, 'M', 'charlie.brown@@@XYZ.com');
