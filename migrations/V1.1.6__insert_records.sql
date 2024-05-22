fd2c8ff2040107e8a722a5d12e1f4ae2ad5c0ad8
Jasraj Gill <jasrajgill2752@gmail.com>
2024-05-22 15:32:48 +0930

    d

--- migrations/V1.1.6__insert_records.sql
+++ migrations/V1.1.6__insert_records.sql
@@ -1,6 +1,6 @@
 USE SCHEMA DEMO;
 INSERT INTO HELLO_WORLD 
-VALUES (Joe, Smith, 32, F, joe.smith@@@XYZ.com);
+VALUES ('Joe', 'Smith', 32, 'F', 'joe.smith@@@XYZ.com');
 
 INSERT INTO HELLO_WORLD 
-VALUES (Alice, ohnson, 28, F, alice.johnson@@@XYZ.com);
+VALUES ('Alice', 'ohnson', 28, 'F', 'lice.johnson@@@XYZ.com');
